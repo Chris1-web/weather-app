1cc07de70027a32c33fdd9bc9f6c223aec4a4217
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-05-01 18:14:04 +0100

    webpack startup completed

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert("hello world");
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "WEATHER APP",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
