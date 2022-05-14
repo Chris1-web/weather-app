93c2275652b73f7d090369f27b2f60221bce3a65
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-05-14 09:31:41 +0100

    UI/UX improve

--- src/loadImages.js
+++ src/loadImages.js
@@ -3,9 +3,7 @@ import searchIcon from "./icons/search.png";
 
 const loadImages = function () {
   const footerIcon = document.querySelector(".contact-github-icon");
-  const searchButton = document.querySelector(".search-icon");
   footerIcon.src = githubIcon;
-  searchButton.src = searchIcon;
 };
 
 export default loadImages;
